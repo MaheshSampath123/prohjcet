60f88dc4b89b30ec295d7e9994b66c6207bfed33
MaheshSampath123 <83757151+MaheshSampath123@users.noreply.github.com>
2021-05-11 06:40:12 +0530

    text added

---
+++ practice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int c;
+            c = a + b;
+            Console.WriteLine("c="+c);
+            Console.ReadKey();
+        }
+    }
+}
